dd6d61c7b71fb02b6ad886c2b8c31f80ea91df81
jbacon <julien.baccon.1@gmail.com>
2021-11-20 17:12:06 -0500

    cleaner long line management

--- src/pkgs/mqttClient/__init__.py
+++ src/pkgs/mqttClient/__init__.py
@@ -1,2 +1,2 @@
-from .client import init, connect, disconnect, startLoop, stopLoop, publish     # noqa: F401 E501
-from .client import subscribe, unscubscribe, registerMsgCallback, unregisterMsgCallback     # noqa: F401 E501
+from .client import init, connect, disconnect, startLoop, stopLoop, publish, \
+    subscribe, unscubscribe, registerMsgCallback, unregisterMsgCallback     # noqa: F401 E501
