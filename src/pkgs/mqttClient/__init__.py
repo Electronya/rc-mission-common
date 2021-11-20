4bae6e45744bf72f0a68b1c8ccf1bfd7556c0f1b
jbacon <julien.baccon.1@gmail.com>
2021-11-20 17:21:39 -0500

    adding exceptions to mqttClient imports

--- src/pkgs/mqttClient/__init__.py
+++ src/pkgs/mqttClient/__init__.py
@@ -1,2 +1,3 @@
 from .client import init, connect, disconnect, startLoop, stopLoop, publish, \
     subscribe, unscubscribe, registerMsgCallback, unregisterMsgCallback     # noqa: F401 E501
+from .exceptions import MqttClientNotInit                                   # noqa: F401 E501
